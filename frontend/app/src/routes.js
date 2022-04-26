7cfe40e43060338b5b6adcdd9c341adb7a43430e
Katsuhide_Minematsu <kuromine62@gmail.com>
2022-04-27 00:35:47 +0900

    Trying to Chakra UI

--- frontend/app/src/routes.js
+++
@@ -1,9 +0,0 @@
-import One from './page/one';
-import Two from './page/two';
-
-const routes = [
-    { path: '/', component: One, exact : true },
-    { path: '/two', component: Two,},
-  ];
-  
-  export default routes;
